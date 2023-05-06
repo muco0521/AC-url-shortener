e0dc506f233a8ed76c293a0328e0672a25a0d1e5
muco0521 <muco0521@gmail.com>
2023-05-06 09:32:06 +0800

    feat: add mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI)
+
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log(error)
+})
+
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
+
+module.exports = db
