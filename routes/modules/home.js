23faf01d0ea51c45dbb459425e97ff5dea023103
muco0521 <muco0521@gmail.com>
2023-05-10 09:55:53 +0800

    feat: show different error messsages

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
   return res.render('index')
 })
 
-//connect to shortUrl
+// 連結短網址到原網址
 router.get("/:path", (req, res) => {
   const path = req.params.path;
   const shortUrl = SERVER + path
@@ -24,22 +24,24 @@ router.post('/', (req, res) => {
   const originalUrl = req.body.originalUrl.trim()
   const shortUrl = SERVER + shortCode()
 
-  //check url valid
+  // 用 validateUrl() 檢查網址是否有效
   if (validateUrl(originalUrl) === false) {
-    return res.render('index', { warning: 'warning' })
+    return res.render('index', { warning: 'The URL is invalid!' })
   }
-  //check the same url
+  
   Url.findOne({ originalUrl })
-    .lean()
-    .then((url) => {
-      if (url) { return res.render('index', { shortUrl: url.shortUrl })}
-
-      return Url.create({ originalUrl, shortUrl })
-        .then(() => res.render('index', { shortUrl }))
-      
+    // 查看資料庫是否有資料，沒有的話新建一個
+    .then((url) => 
+       url ?  url : Url.create({ originalUrl, shortUrl }) 
+    )
+    // 有資料的話，渲染原資料畫面
+    .then((url) => 
+       res.render('index', { shortUrl: url.shortUrl })
+    )
+    .catch(err => {
+      console.error(err)
+      res.render('index', { warning: 'Sorry there was a problem with your request.' })
     })
-    .catch((error) => console.log(error))
 })
 
 module.exports = router
-
